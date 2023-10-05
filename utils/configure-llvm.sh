08dfb15f20237eb712a85df5181b5ca198dc4e05
gilsaia <794433219@qq.com>
2023-10-06 00:00:18 +0800

    [fix] tmp test

--- utils/configure-llvm.sh
+++ utils/configure-llvm.sh
@@ -2,7 +2,7 @@ mkdir 3rdparty/llvm-project/build
 cd 3rdparty/llvm-project/build
 cmake -G Ninja ../llvm \
    -DLLVM_ENABLE_PROJECTS=mlir \
-   -DCMAKE_BUILD_TYPE=Release \
+   -DCMAKE_BUILD_TYPE=RelWithDebInfo \
    -DLLVM_ENABLE_ASSERTIONS=ON \
    -DLLVM_ENABLE_RTTI=ON \
    -DLLVM_ENABLE_LIBEDIT=OFF
