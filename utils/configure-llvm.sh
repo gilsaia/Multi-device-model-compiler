7d8234a9440fcea874ed079528d529ab3a524f8b
gilsaia <794433219@qq.com>
2023-10-05 18:50:26 +0800

    [add] add llvm configure

---
+++ utils/configure-llvm.sh
@@ -0,0 +1,8 @@
+mkdir 3rdparty/llvm-project/build
+cd 3rdparty/llvm-project/build
+cmake -G Ninja ../llvm \
+   -DLLVM_ENABLE_PROJECTS=mlir \
+   -DCMAKE_BUILD_TYPE=Release \
+   -DLLVM_ENABLE_ASSERTIONS=ON \
+   -DLLVM_ENABLE_RTTI=ON \
+   -DLLVM_ENABLE_LIBEDIT=OFF
