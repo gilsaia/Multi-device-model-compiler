401457bbb6bc6cf4080509270c4db9640850baa9
gilsaia <794433219@qq.com>
2023-10-06 18:26:44 +0800

    [fix] change onnx-mlir gnu pos

--- utils/configure-llvm.sh
+++ utils/configure-llvm.sh
@@ -2,7 +2,7 @@ mkdir 3rdparty/llvm-project/build
 cd 3rdparty/llvm-project/build
 cmake -G Ninja ../llvm \
    -DLLVM_ENABLE_PROJECTS=mlir \
-   -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+   -DCMAKE_BUILD_TYPE=Release \
    -DLLVM_ENABLE_ASSERTIONS=ON \
    -DLLVM_ENABLE_RTTI=ON \
    -DLLVM_ENABLE_LIBEDIT=OFF
