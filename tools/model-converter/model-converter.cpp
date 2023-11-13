296a413fc8ce22c807896adffa649c991c70f00f
gilsaia <794433219@qq.com>
2023-11-13 16:24:07 +0800

    [add] output onnx base

--- tools/model-converter/model-converter.cpp
+++ tools/model-converter/model-converter.cpp
@@ -3,6 +3,7 @@
 #include "llvm/Support/CommandLine.h"
 
 #include "Builder/FrontendDialectTransformer.hpp"
+#include "Compiler/CompilerUtils.hpp"
 
 int main(int argc, char **argv) {
   llvm::cl::opt<std::string> inputFilename(
@@ -16,6 +17,30 @@ int main(int argc, char **argv) {
     llvm::errs() << "Failed to parse options\n";
     return 1;
   }
+
+  mlir::MLIRContext context;
+  onnx_mlir::registerDialects(context);
+
+  mlir::OwningOpRef<mlir::ModuleOp> module;
+  std::string errorMessage;
+  int rc = onnx_mlir::processInputFile(inputFilename, context, module,
+                                       &errorMessage);
+  if (rc != 0) {
+    if (!errorMessage.empty()) {
+      llvm::errs() << errorMessage << "\n";
+      return 1;
+    }
+  }
+
+  if (outputBaseName == "") {
+    outputBaseName = inputFilename.substr(0, inputFilename.find_last_of("."));
+  }
+
+  std::string outputNameWithExt = outputBaseName + ".onnx.mlir";
+  std::string outputTmpWithExt = outputBaseName + ".tmp";
+  onnx_mlir::outputCode(module, outputNameWithExt);
+  // Elide element attributes if larger than 100
+  onnx_mlir::outputCode(module, outputTmpWithExt, 100);
 
   return 0;
   // mlir::DialectRegistry registry;
