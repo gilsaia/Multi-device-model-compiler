8db546c19459008422b5e9436908eecc4c0e5c8c
gilsaia <794433219@qq.com>
2023-12-21 12:35:53 +0800

    [add] basic ci pipeline

---
+++ utils/build-llvm.sh
@@ -0,0 +1,3 @@
+cd 3rdparty/llvm-project
+cmake --build .
+cmake --build . --target check-mlir
--- utils/configure-llvm.sh
+++ utils/configure-llvm.sh
@@ -1,7 +1,7 @@
 mkdir 3rdparty/llvm-project/build
 cd 3rdparty/llvm-project/build
 cmake -G Ninja ../llvm \
-   -DLLVM_ENABLE_PROJECTS="mlir;clang;lld" \
+   -DLLVM_ENABLE_PROJECTS="mlir" \
    -DLLVM_BUILD_EXAMPLES=ON \
    -DLLVM_TARGETS_TO_BUILD="X86;NVPTX;AMDGPU" \
    -DCMAKE_BUILD_TYPE=Release \
