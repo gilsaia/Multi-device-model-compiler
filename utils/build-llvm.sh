7870211736a550594758fca4d0565a0e84140511
gilsaia <794433219@qq.com>
2023-12-22 20:52:24 +0800

    [add] add ci file (#3)
    * [add] basic ci pipeline
    
    * [fix] build dir change
    
    * [fix] change ccache usage
    
    * [fix] install onnx
    
    * [fix] continue-on-error tmp
    
    * [fix] change install onnx
    
    * [fix] change onnx install
    
    * [fix] try onnx install proto
    
    * [fix] change protobuf
    
    * [fix] change sudo
    
    * [fix] use ccache
    
    * [fix] final config

---
+++ utils/build-llvm.sh
@@ -0,0 +1,3 @@
+cd 3rdparty/llvm-project/build
+cmake --build .
+cmake --build . --target check-mlir
---
+++ utils/build-protobuf.sh
@@ -0,0 +1,7 @@
+cd protobuf
+mkdir build_source
+cd build_source
+cmake ../cmake -Dprotobuf_BUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_INSTALL_SYSCONFDIR=/etc -DCMAKE_POSITION_INDEPENDENT_CODE=ON -Dprotobuf_BUILD_TESTS=OFF -DCMAKE_BUILD_TYPE=Release
+make -j$(nproc)
+sudo make install
+
