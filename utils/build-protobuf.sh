7f136ebbf6e7f2ae00989cd004239e69a5f7d068
gilsaia <794433219@qq.com>
2023-12-22 18:06:30 +0800

    [fix] change sudo

--- utils/build-protobuf.sh
+++ utils/build-protobuf.sh
@@ -3,5 +3,5 @@ mkdir build_source
 cd build_source
 cmake ../cmake -Dprotobuf_BUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_INSTALL_SYSCONFDIR=/etc -DCMAKE_POSITION_INDEPENDENT_CODE=ON -Dprotobuf_BUILD_TESTS=OFF -DCMAKE_BUILD_TYPE=Release
 make -j$(nproc)
-make install
+sudo make install
 
