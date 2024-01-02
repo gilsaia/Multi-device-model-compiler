d6f498489a4fd70032053943581a202e6572771d
gilsaia <794433219@qq.com>
2024-01-02 14:58:54 +0800

    [add] device dialect

---
+++ include/multi-device-model-compiler/Dialect/Device/IR/Device.h
@@ -0,0 +1,16 @@
+#ifndef MULTI_DEVICE_MODEL_COMPILER_DIALECT_DEVICE_IR_DEVICE_H_
+#define MULTI_DEVICE_MODEL_COMPILER_DIALECT_DEVICE_IR_DEVICE_H_
+
+#include "mlir/IR/Attributes.h"
+#include "mlir/IR/Dialect.h"
+#include "llvm/ADT/StringExtras.h"
+
+#include "multi-device-model-compiler/Dialect/Device/IR/DeviceOpsDialect.h.inc"
+
+#include "multi-device-model-compiler/Dialect/Device/IR/DeviceOpsEnums.h.inc"
+#define GET_ATTRDEF_CLASSES
+#include "multi-device-model-compiler/Dialect/Device/IR/DeviceOpsAttributes.h.inc"
+#define GET_OP_CLASSES
+#include "multi-device-model-compiler/Dialect/Device/IR/DeviceOps.h.inc"
+
+#endif
