d28868d2eded70a1de924a66e0a34f956873f8c7
gilsaia <794433219@qq.com>
2024-01-24 21:11:06 +0800

    [fix] change data util pos

--- benchmark/CPU/onnxruntime_run.py
+++ benchmark/CPU/onnxruntime_run.py
@@ -3,33 +3,14 @@ import onnxruntime as ort
 
 import argparse
 import time
+import sys
+import os
 
-
-def parse_type(type: str):
-    if type == "f32":
-        dtype = np.float32
-    else:
-        raise NotImplementedError("Not Implemented type!")
-    return dtype
-
-
-def load_data_raw(file_name: str, type: str):
-    dtype = parse_type(type)
-    data = np.fromfile(file_name, dtype=dtype, sep="")
-    return data
-
-
-def print_shape(shapes: list[int]):
-    suffix = ""
-    for s in shapes:
-        suffix += str(s) + "_"
-    suffix = suffix.removesuffix("_")
-    return suffix
-
-
-def save_data(array: np.ndarray, output_name: str):
-    file_name = f"{output_name}_{array.dtype}_{print_shape(array.shape)}.dat"
-    array.tofile(file_name)
+# 将data_util目录添加到sys.path
+current_dir = os.path.dirname(os.path.abspath(__file__))
+root_dir = os.path.join(current_dir, "..")
+sys.path.append(root_dir)
+from data_util import load_data_raw, save_data
 
 
 def profile_model(model: str, args):
